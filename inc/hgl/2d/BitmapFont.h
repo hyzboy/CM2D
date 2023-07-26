ef84a87f238b1a445db6be97274cf2b5ca5aa5e1
HuYingzhuo(hugo/hyzboy) <hyzboy@gmail.com>
2023-07-26 22:29:49 +0800

    added BitmapFont.h

---
+++ inc/hgl/2d/BitmapFont.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include<hgl/type/DataType.h>
+
+namespace hgl
+{
+    /*
+    * 基于位图的字体
+    */
+    class BitmapFont
+    {
+        uint bits;
+        uint width,height;
+
+    public:
+
+        
+    };//class BitmapFont
+}//namespace hgl
