03b9fe120268cc1458ed3ac0c96adf7cf858af06
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-01-28 20:58:15 +0800

    改进cmake路径配置

--- path_config.cmake
+++ path_config.cmake
@@ -1,9 +1,8 @@
 macro(CM2DSetup CM2D_ROOT_PATH)
+    message("CM2D root path: " ${CM2D_ROOT_PATH})
 
-    message("CM2D_ROOT_PATH: " ${CM2D_ROOT_PATH})
-
-    set(CM2D_ROOT_INCLUDE_PATH    ${CM2D_ROOT_PATH}/inc)
-    set(CM2D_ROOT_SOURCE_PATH     ${CM2D_ROOT_PATH}/src)
+    set(CM2D_ROOT_INCLUDE_PATH ${CM2D_ROOT_PATH}/inc)
+    set(CM2D_ROOT_SOURCE_PATH ${CM2D_ROOT_PATH}/src)
 
     include_directories(${CM2D_ROOT_INCLUDE_PATH})
 endmacro()
